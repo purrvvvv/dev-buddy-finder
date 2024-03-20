283ed3b58a94de255dad71e48843a4955b721e48
purrvvvv <purvshah10@gmail.com>
2024-03-20 17:52:55 +0530

    check

--- src/db/index.ts
+++ src/db/index.ts
@@ -4,6 +4,6 @@ import * as schema from "./schema";
 import postgres from 'postgres';
 
 const queryClient = postgres(process.env.DATABASE_URL !);
-const db = drizzle(queryClient, {schema});
+const db = drizzle(queryClient,{schema});
 
 export{db};
